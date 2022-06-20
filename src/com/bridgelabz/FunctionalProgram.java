3014ea3c87543b0c540f72364ba07a063d6d45f9
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-20 16:55:17 +0530

    Added: Welcome message for Functional Programs.

---
+++ src/com/bridgelabz/FunctionalProgram.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class FunctionalProgram {
+    public static void main(String args[]) {
+        System.out.println("Welcome to check some Functional programs.");
+    }
+}
